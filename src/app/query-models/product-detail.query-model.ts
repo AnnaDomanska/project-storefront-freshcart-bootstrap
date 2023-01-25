6b5ad7fedce5f7f4a915acb897009642cf270636
annah <anna.h.domanska@gmail.com>
2023-01-25 09:49:38 +0100

    feat: product-detail query model created

---
+++ src/app/query-models/product-detail.query-model.ts
@@ -0,0 +1,20 @@
+export interface ProductDetailQueryModel {
+  readonly name: string;
+  readonly price: number;
+  readonly imageUrl: string
+  readonly priceBeforeDiscount: number;
+  readonly discountInPercentages: number;
+  readonly ratingValue: number;
+  readonly ratingCount: number;
+  readonly categoryName: string;
+  readonly stores: {
+    readonly name: string,
+    readonly logoUrl: string
+    readonly id: string
+  }[];
+  readonly relatedProducts: {
+    readonly name: string;
+    readonly imageUrl: string
+    readonly price: number
+  }[];
+}
