483e7bc20a04d8555c8b76ab46ab9f0d44f869f5
annah <anna.h.domanska@gmail.com>
2023-01-27 22:54:28 +0100

    feat: shopping-cart service created

--- src/app/services/shopping-cart.service.ts
+++ src/app/services/shopping-cart.service.ts
@@ -1,22 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject, tap, map } from 'rxjs';
+import { ProductDetailComponent } from '../components/product-detail/product-detail.component';
 import { ProductModel } from '../models/product.model';
+import { ProductDetailQueryModel } from '../query-models/product-detail.query-model';
 
 @Injectable({ providedIn: 'root' })
 export class ShoppingCartService {
-  private _productsInCartSubject: BehaviorSubject<ProductModel[]> =
-    new BehaviorSubject<ProductModel[]>([]);
-  public productsInCart$: Observable<ProductModel[]> =
+  private _productsInCartSubject: BehaviorSubject<ProductDetailQueryModel[]> =
+    new BehaviorSubject<ProductDetailQueryModel[]>([]);
+  public productsInCart$: Observable<ProductDetailQueryModel[]> =
     this._productsInCartSubject.asObservable();
 
-  private addProductToCart(product: ProductModel) {
+  addProductToCart(product: ProductDetailQueryModel) {
     this._productsInCartSubject.next([
       ...this._productsInCartSubject.value,
       product,
     ]);
+    console.log(product.name + ' added')
   }
-  
-  private removeProductFromCart(removedProduct: ProductModel) {
+
+  removeProductFromCart(removedProduct: ProductDetailQueryModel) {
     this._productsInCartSubject.next([
       ...this._productsInCartSubject.value.filter(
         (product) => product.id !== removedProduct.id
